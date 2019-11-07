e16fcb45860e8980a5eff15590071d3a551b1eea
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-11-07 16:36:19 +1300

    support port assignment via PORTAINER_PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ echo "Deploying Portainer"
 
 docker service create --name integration_portainer \
 --network portainer_agent_network \
---publish 9100:9000 \
---publish 8000:8000 \
+--publish ${PORTAINER_PORT:-9100}:9000 \
+# --publish 8000:8000 \
 --replicas=1 \
 --mount type=bind,src=//tmp/integration,dst=/data \
 --constraint 'node.role == manager' \
